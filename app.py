c243c391156eb2135c1791f32d5d95d362a66b54
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2023-05-20 22:14:18 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,242 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+from config import config
+
+# Models:
+from models.ModelUser import ModelUser
+
+# Entities:
+from models.entities.User import User
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = config['development'].MYSQL_HOST
+app.config['MYSQL_USER'] = config['development'].MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config['development'].MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config['development'].MYSQL_DB
+app.config['SECRET_KEY'] = config['development'].SECRET_KEY
+
+db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db, id)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User(0, request.form['username'], request.form['password'], "")
+        logged_user = ModelUser.login(db, user)
+        if logged_user != None:
+            if logged_user.password:
+                login_user(logged_user)
+                return redirect(url_for('home'))
+            else:
+                flash("Invalid password...")
+                return render_template('auth/login.html')
+        else:
+            flash("User not found...")
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+#++++++++++++++++++++ Estados ++++++++++++++++++++
+@app.route('/estados')
+@login_required
+def estados():
+    cursor = db.connection.cursor()
+    sql = """SELECT id, name, state FROM estados"""
+    cursor.execute(sql)
+    estados = cursor.fetchall()
+
+    return render_template('catalogos/estados.html', estados=estados)
+
+
+@app.route('/new-estado', methods=['POST'])
+@login_required
+def newEstados():
+    cursor = db.connection.cursor()
+    name = request.form['name']
+    if request.method == 'POST':
+        sqlIns = 'INSERT INTO estados (name) VALUES ("{}")'.format(name)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('estados'))
+
+
+@app.route('/update-estado/<int:id>', methods=['POST'])
+@login_required
+def updateEstados(id):
+    cursor = db.connection.cursor()
+    name = request.form['update-name']
+    if request.method == 'POST':
+        sqlIns = 'UPDATE estados SET name = "{}" WHERE id = {}'.format(name, id)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('estados'))
+
+#++++++++++++++++++++ Municipios ++++++++++++++++++++
+@app.route('/municipios')
+@login_required
+def municipios():
+    cursor = db.connection.cursor()
+    sql = """SELECT m.id,m.name,e.name,m.state,m.idEstado FROM municipio m 
+    INNER JOIN estados e ON e.id = m.idEstado"""
+    cursor.execute(sql)
+    municipios = cursor.fetchall()
+
+    cursor = db.connection.cursor()
+    sql = """SELECT id, name, state FROM estados WHERE state = 1"""
+    cursor.execute(sql)
+    estados = cursor.fetchall()
+
+    return render_template('catalogos/municipios.html', municipios=municipios, estados=estados)
+
+
+@app.route('/new-municipio', methods=['POST'])
+@login_required
+def newMunicipio():
+    cursor = db.connection.cursor()
+    name = request.form['name']
+    idEstado = request.form.get('estados')
+    if request.method == 'POST':
+        sqlIns = 'INSERT INTO municipio (name, idEstado) VALUES ("{}", {})'.format(name, idEstado)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('municipios'))
+
+
+@app.route('/update-municipio/<int:id>', methods=['POST'])
+@login_required
+def updateMunicipio(id):
+    cursor = db.connection.cursor()
+    name = request.form['update-name']
+    state = request.form.get('btnradio')
+    idEstado = request.form.get('inputEstados')
+    if request.method == 'POST':
+        sqlIns = 'UPDATE municipio SET name = "{}",idEstado = {}, state = {} WHERE id = {}'.format(name, idEstado, state, id)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('municipios'))
+
+
+#++++++++++++++++++++ Colonia ++++++++++++++++++++
+@app.route('/colonias')
+@login_required
+def colonias():
+    cursor = db.connection.cursor()
+    sql = """SELECT c.id,c.name,m.name,c.cp,c.state,c.idMunicipio FROM colonia c
+    INNER JOIN municipio m ON m.id = c.idMunicipio"""
+    cursor.execute(sql)
+    colonias = cursor.fetchall()
+
+    sql = """SELECT id, name, state FROM municipio WHERE state = 1"""
+    cursor.execute(sql)
+    municipios = cursor.fetchall()
+
+    return render_template('catalogos/colonias.html', colonias=colonias, municipios=municipios)
+
+
+@app.route('/new-colonia', methods=['POST'])
+@login_required
+def newColonia():
+    cursor = db.connection.cursor()
+    name = request.form['name']
+    idMunicipio = request.form.get('municipio')
+    cp = request.form['cp']
+    if request.method == 'POST':
+        sqlIns = 'INSERT INTO colonia (name, idMunicipio, cp) VALUES ("{}", {}, "{}")'.format(name, idMunicipio, cp)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('colonias'))
+
+
+@app.route('/update-colonia/<int:id>', methods=['POST'])
+@login_required
+def updateColonia(id):
+    cursor = db.connection.cursor()
+    name = request.form['update-name']
+    state = request.form.get('btnradio')
+    idMunicipio = request.form.get('inputMunicipio')
+    cp = request.form['update-cp']
+    if request.method == 'POST':
+        sqlIns = 'UPDATE colonia SET name = "{}", idMunicipio = {}, cp = {}, state = {} WHERE id = {}'.format(name, idMunicipio, cp, state, id)
+        cursor.execute(sqlIns)
+        db.connection.commit()
+
+    return redirect(url_for('colonias'))
+
+#++++++++++++++++++++ Escuelas ++++++++++++++++++++
+#TODO: registrar y editar
+@app.route('/escuelas')
+@login_required
+def escuelas():
+    cursor = db.connection.cursor()
+    sql = """SELECT e.id, e.name, c.name, e.street, e.number, e.cp, e.state FROM escuela e 
+    INNER JOIN colonia c ON c.id = e.idColonia"""
+    cursor.execute(sql)
+    escuelas = cursor.fetchall()
+    
+    sql = """SELECT id, name, state FROM colonia WHERE state = 1"""
+    cursor.execute(sql)
+    colonias = cursor.fetchall()
+
+    return render_template('catalogos/escuelas.html', escuelas=escuelas, colonias=colonias)
+
+#++++++++++++++++++++ Docentes ++++++++++++++++++++
+#TODO: consultar, registrar y editar, asi como la creación de la tabla
+@app.route('/docentes')
+@login_required
+def docentes():
+    cursor = db.connection.cursor()
+    sql = """SELECT m.id, m.name, e.name, m.state FROM municipio m 
+    INNER JOIN estados e ON e.id = m.idEstado"""
+    cursor.execute(sql)
+    docentes = cursor.fetchall()
+    print(estados)
+
+    return render_template('catalogos/docentes.html', docentes=docentes)
+
+
+#++++++++++++++++++++ Alumnos ++++++++++++++++++++
+
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>", 404
+
+
+if __name__ == '__main__':
+    app.register_error_handler(401, status_401)
+    app.register_error_handler(404, status_404)
+    app.run(debug=True)
---
+++ database/flask_login.sql
@@ -0,0 +1,40 @@
+-- Creación de la table de user
+
+CREATE TABLE user (
+  id smallint(3) NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
+  password char(102) COLLATE utf8_unicode_ci NOT NULL,
+  fullname varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Creación de la table de colonia,
+-- esta relacionada con Municipio
+
+CREATE TABLE colonia (
+    id int NOT NULL AUTO_INCREMENT,
+    idMunicipio int,
+    name varchar(100) NOT NULL,
+    cp varchar(5),
+    state tinyint(1) default 1,
+    PRIMARY KEY (id)
+); 
+
+-- Creación de la table de escuela
+-- esta realcionada con Colonia
+
+CREATE TABLE escuela (
+    id int NOT NULL AUTO_INCREMENT,
+    idColonia int,
+    cp varchar(5),
+    name varchar(100) NOT NULL,
+    street varchar(200) NOT NULL,
+    number varchar(10) NOT NULL,
+    state tinyint(1) default 1,
+    PRIMARY KEY (id)
+); 
+
+-- Volcado de datos para la tabla user
+
+INSERT INTO user (id, username, password, fullname) VALUES
+(1, 'CCARDENAS', 'pbkdf2:sha256:260000$fiRyeVmApEki8uvm$40d93cdea3f941010f3eedfafd228db15c41810ce625a691ff8cb2491300b010', 'Cesar Cardenas');
